334e42443b0c1c06791096ff39b17b8c5ce2efcd
Surafeljava <surafelk27@gmail.com>
2022-02-03 19:17:11 +0300

    feat: home page design done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,107 @@
 import { Grid, CssBaseline, Button, IconButton, Typography } from '@mui/material';
-import KeyboardCommandKeyIcon from '@mui/icons-material/KeyboardCommandKey';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+import { makeStyles } from "@mui/styles";
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
 
 import './app.css';
 
+const useStyles = makeStyles((theme) => ({
+  topbutton: {
+    color: "#CCCCCC",
+    fontSize: "18px",
+    fontWeight: 100,
+    letterSpacing: 5,
+
+    "&:hover": {
+      color: "#FFD016"
+    }
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <Grid container direction="column" width="100%" height="100vh" style={{backgroundColor: "#040710"}} justifyContent="center">
+    <Grid container direction="column" width="100%" height="100vh" style={{backgroundColor: "#040710", padding: "20px"}} justifyContent="center">
       <CssBaseline/>
       <Grid container direction="row" justifyContent="center">
         <Button style={{color:"#FFFFFF"}} variant="text">
           <div className="font-face-gm">
-            <h3>
-              MY WORKS
-            </h3>
+            <Typography variant="h5" fontFamily= "Manifesto" className={classes.topbutton}>  MY WORKS </Typography>
           </div>
         </Button>
 
         &nbsp;&nbsp;&nbsp;
 
         <IconButton>
-          <KeyboardCommandKeyIcon color="secondary"/>
+          <CircleOutlinedIcon style={{fontSize: "20px", color: "#FFD016DD"}}/>
         </IconButton>
 
         &nbsp;&nbsp;&nbsp;
 
         <Button style={{color:"#FFFFFF"}} variant="text" className="font-face-gm">
           <div className="font-face-gm">
-            <h3>
-              CONTACT
-            </h3>
+            <Typography variant="h5" className={classes.topbutton} fontFamily="Manifesto">  CONTACT </Typography>
           </div>
         </Button>
       </Grid>
-      <Grid container direction="row" flexGrow={1} alignItems="center" justifyContent="center">
-        <Grid item xs={2}>
-          <h3 style={{color:"#FFFFFF"}}> Year </h3>
+      <Grid container direction="row" flexGrow={1} alignItems="center" justifyContent="space-between">
+        <Grid item width="80px">
+          <Typography variant="h5" color="#CCCCCC" component="div" fontSize={25} fontWeight={100} 
+          letterSpacing={10} fontFamily="Roboto" style={{transform: "rotate(-90deg)"}}>  2022 </Typography>
         </Grid>
-        <Grid item xs={8}>
-          <div className="font-face-gm">
-            <Typography  className="font-face-gm" style={{color:"#FFFFFF", fontSize: "100px"}}> SURAFEL KINDU </Typography>
-          </div>
+
+
+        <Grid container direction="column" alignItems="center" xs={8}>
+          
+          <Grid container direction="row" justifyContent="center" alignItems="center">
+            <CircleOutlinedIcon color="secondary"/>
+            
+            <Typography color="#FFFFFF" fontSize= {100} fontFamily= "Manifesto" marginLeft={4} marginRight={4}> SURAFEL KINDU </Typography>
+            
+            <CircleOutlinedIcon color="secondary"/>
+          </Grid>
+
+          <ArrowDownwardIcon style={{fontSize: "30px", color: "#FFFFFFAA"}}/>
+          <br />
+
+          <Typography variant="h5" color="#FFD016CC" fontSize={28} fontWeight={100} letterSpacing={20} fontFamily="Roboto">  GRAPHICS DESIGNER </Typography>
+          
+
         </Grid>
-        <Grid item xs={2}>
-          <h3 style={{color:"#FFFFFF"}}> Socials </h3>
+
+
+        <Grid container direction="column" width="80px">
+          <IconButton onClick={() => {
+            window.open("https://www.instagram.com/surafel_kindu/");
+          }}>
+            <FacebookIcon style={{fontSize: "33px", color: "#FFFFFF"}}/>
+          </IconButton>
+
+          <br />
+
+          <IconButton onClick={() => {
+            window.open("https://www.instagram.com/surafel_kindu/");
+          }}>
+            <InstagramIcon style={{fontSize: "33px", color: "#FFFFFF"}}/>
+          </IconButton>
+
+          <br />
+          
+          <IconButton onClick={() => {
+            window.open("https://twitter.com/surafel_kindu");
+          }}>
+            <TwitterIcon style={{fontSize: "33px", color: "#FFFFFF"}}/>
+          </IconButton>
+          
         </Grid>
       </Grid>
+
+
     </Grid>
   );
 }
