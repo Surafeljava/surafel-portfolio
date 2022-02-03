96aff7c6b79ea34c9c94d9b33cd4fcc3a2ba3db7
Surafeljava <surafelk27@gmail.com>
2022-02-03 16:40:12 +0300

    feat: creating theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { Grid, CssBaseline, Button, IconButton, Typography } from '@mui/material';
+import KeyboardCommandKeyIcon from '@mui/icons-material/KeyboardCommandKey';
+
+import './app.css';
 
 function App() {
   return (
-    <div>
-      <h1> Surafel Kindu Portfolio </h1>
-    </div>
+    <Grid container direction="column" width="100%" height="100vh" style={{backgroundColor: "#040710"}} justifyContent="center">
+      <CssBaseline/>
+      <Grid container direction="row" justifyContent="center">
+        <Button style={{color:"#FFFFFF"}} variant="text">
+          <div className="font-face-gm">
+            <h3>
+              MY WORKS
+            </h3>
+          </div>
+        </Button>
+
+        &nbsp;&nbsp;&nbsp;
+
+        <IconButton>
+          <KeyboardCommandKeyIcon color="secondary"/>
+        </IconButton>
+
+        &nbsp;&nbsp;&nbsp;
+
+        <Button style={{color:"#FFFFFF"}} variant="text" className="font-face-gm">
+          <div className="font-face-gm">
+            <h3>
+              CONTACT
+            </h3>
+          </div>
+        </Button>
+      </Grid>
+      <Grid container direction="row" flexGrow={1} alignItems="center" justifyContent="center">
+        <Grid item xs={2}>
+          <h3 style={{color:"#FFFFFF"}}> Year </h3>
+        </Grid>
+        <Grid item xs={8}>
+          <div className="font-face-gm">
+            <Typography  className="font-face-gm" style={{color:"#FFFFFF", fontSize: "100px"}}> SURAFEL KINDU </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={2}>
+          <h3 style={{color:"#FFFFFF"}}> Socials </h3>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import './index.css';
+
+import './fonts/Manifesto/MANIFESTO.ttf';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#040710" 
+        },
+        secondary: {
+            main: "#FFD016"
+        }
+    },
+    typography: {
+      button: {
+        textTransform: 'none'
+      }
+    }
+    
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
