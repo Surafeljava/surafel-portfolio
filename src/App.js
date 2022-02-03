ea49ac9003c2c2daa80fd39a69dab72ccd6e3d99
Surafeljava <surafelk27@gmail.com>
2022-02-03 20:05:03 +0300

    feat: responsiveness done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Grid, CssBaseline, Button, IconButton, Typography } from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
@@ -13,26 +15,35 @@ import './app.css';
 const useStyles = makeStyles((theme) => ({
   topbutton: {
     color: "#CCCCCC",
-    fontSize: "18px",
     fontWeight: 100,
     letterSpacing: 5,
-
     "&:hover": {
       color: "#FFD016"
+    }
+  },
+  maintext: {
+    color: "#FFFFFF",
+    "&:hover": {
+      fontSize: 100
     }
   }
 }));
 
 function App() {
   const classes = useStyles();
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isDesktop = useMediaQuery({ minWidth: 768 })
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+
   return (
     <Grid container direction="column" width="100%" height="100vh" style={{backgroundColor: "#040710", padding: "20px"}} justifyContent="center">
       <CssBaseline/>
       <Grid container direction="row" justifyContent="center">
         <Button style={{color:"#FFFFFF"}} variant="text">
-          <div className="font-face-gm">
-            <Typography variant="h5" fontFamily= "Manifesto" className={classes.topbutton}>  MY WORKS </Typography>
-          </div>
+          <Typography variant="h5" fontFamily= "Manifesto" className={classes.topbutton} fontSize={isMobile ? 18 : 22}>  MY WORKS </Typography>
         </Button>
 
         &nbsp;&nbsp;&nbsp;
@@ -44,38 +55,47 @@ function App() {
         &nbsp;&nbsp;&nbsp;
 
         <Button style={{color:"#FFFFFF"}} variant="text" className="font-face-gm">
-          <div className="font-face-gm">
-            <Typography variant="h5" className={classes.topbutton} fontFamily="Manifesto">  CONTACT </Typography>
-          </div>
+          <Typography variant="h5" className={classes.topbutton} fontFamily="Manifesto" fontSize={isMobile ? 18 : 22}>  CONTACT </Typography>
         </Button>
       </Grid>
       <Grid container direction="row" flexGrow={1} alignItems="center" justifyContent="space-between">
-        <Grid item width="80px">
-          <Typography variant="h5" color="#CCCCCC" component="div" fontSize={25} fontWeight={100} 
-          letterSpacing={10} fontFamily="Roboto" style={{transform: "rotate(-90deg)"}}>  2022 </Typography>
-        </Grid>
+        {isDesktop && (
+          <Grid item width="80px">
+            <Typography variant="h5" color="#CCCCCC" component="div" fontSize={25} fontWeight={100} 
+            letterSpacing={10} fontFamily="Roboto" style={{transform: "rotate(-90deg)"}}>  2022 </Typography>
+          </Grid>
+        )}
 
-
-        <Grid container direction="column" alignItems="center" xs={8}>
+        <Grid container direction="column" alignItems="center" xs={isMobile ? 12 : 8}>
           
-          <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Grid container direction={ isMobile ? "column" : "row"} justifyContent="center" alignItems="center" alignContents="center">
             <CircleOutlinedIcon color="secondary"/>
+            <br />
             
-            <Typography color="#FFFFFF" fontSize= {100} fontFamily= "Manifesto" marginLeft={4} marginRight={4}> SURAFEL KINDU </Typography>
+            {isDesktop && (
+              <Typography className={classes.maintext} fontFamily= "Manifesto" marginLeft= {4} marginRight= {4} fontSize= {100} > SURAFEL KINDU </Typography>
+            )}
+
+            {isMobile && (
+              <Typography className={classes.maintext} fontFamily= "Manifesto" marginLeft= {4} marginRight= {4} fontSize= {40} align='center'> SURAFEL KINDU </Typography>
+            )}
+
+            <br />
             
             <CircleOutlinedIcon color="secondary"/>
+            <br />
           </Grid>
 
-          <ArrowDownwardIcon style={{fontSize: "30px", color: "#FFFFFFAA"}}/>
+          <ArrowDownwardIcon style={{fontSize: "30px", color: "#FFFFFF55"}}/>
           <br />
 
-          <Typography variant="h5" color="#FFD016CC" fontSize={28} fontWeight={100} letterSpacing={20} fontFamily="Roboto">  GRAPHICS DESIGNER </Typography>
+          <Typography color="#FFD016CC" fontSize={ isMobile ? 20 : 28} fontWeight={100} letterSpacing={20} fontFamily="Roboto" align="center">  GRAPHICS DESIGNER </Typography>
           
 
         </Grid>
 
 
-        <Grid container direction="column" width="80px">
+        <Grid container direction={ isDesktop ? "column" : "row"} width={ isDesktop ? "80px" : "100%"} justifyContent="center">
           <IconButton onClick={() => {
             window.open("https://www.instagram.com/surafel_kindu/");
           }}>
